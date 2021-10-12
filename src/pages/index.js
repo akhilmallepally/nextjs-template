785c303652e6434b0e55a01cebcb83f99e279164
akhilmallepally <akhilnani831@gmail.com>
2021-10-12 01:25:06 -0500

    nextjs boilerplate with tailwindcss

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import Header from "../components/Header"
+
+const Index = () => {
+  return (
+    <div>
+      <Header />
+      <p className="text-3xl">Index page</p>
+    </div>
+  )
+}
+
+export default Index
